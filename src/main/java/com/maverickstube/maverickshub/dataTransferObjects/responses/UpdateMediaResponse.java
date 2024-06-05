55ea0152965675b522f7e6073f34fcd861afa18e
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-05 10:02:05 +0100

    java

--- src/main/java/com/maverickstube/maverickshub/dataTransferObjects/responses/UpdateMediaResponse.java
+++ src/main/java/com/maverickstube/maverickshub/dataTransferObjects/responses/UpdateMediaResponse.java
@@ -1,4 +1,16 @@
 package com.maverickstube.maverickshub.dataTransferObjects.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.maverickstube.maverickshub.models.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
 public class UpdateMediaResponse {
+    @JsonProperty("media_description")
+    private String newDescription;
+    @JsonProperty("category")
+    private Category newCategory;
 }
