4e6274cac9cc5d3a50de5cb056c295a0c48f2097
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-05 17:46:12 +0100

    java

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -8,9 +8,9 @@ insert into users(id,email,password, time_created) values
 (203,'john3@email.com', 'password','2024-06-04T15:03:03.792009700');
 
 
-insert into media(id,category, description, url, time_created) values
-(100,'ACTION','media 1', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700'),
-(101,'ROMANCE','media 2', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700'),
-(102,'STEP_MOM','media 3', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700'),
-(103,'COMEDY','media 4', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700'),
-(104,'SCI_FI','media 5', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700');
+insert into media(id,category, description, url, time_created, uploader_Id) values
+(100,'ACTION','media 1', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700',200),
+(101,'ROMANCE','media 2', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700', 200),
+(102,'STEP_MOM','media 3', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700',200),
+(103,'COMEDY','media 4', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700',201),
+(104,'SCI_FI','media 5', 'https:www.cloudinary.com/media1', '2024-06-04T15:03:03.792009700',201);
