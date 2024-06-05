0889e3d918383ac04480dcdd59191e65383e9cb1
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-05 10:01:16 +0100

    java

--- src/main/java/com/maverickstube/maverickshub/services/MediaService.java
+++ src/main/java/com/maverickstube/maverickshub/services/MediaService.java
@@ -1,9 +1,13 @@
 package com.maverickstube.maverickshub.services;
 
+import com.maverickstube.maverickshub.dataTransferObjects.requests.UpdateMediaRequest;
 import com.maverickstube.maverickshub.dataTransferObjects.requests.UploadMediaRequest;
+import com.maverickstube.maverickshub.dataTransferObjects.responses.UpdateMediaResponse;
 import com.maverickstube.maverickshub.dataTransferObjects.responses.UploadMediaResponse;
 import com.maverickstube.maverickshub.models.Media;
 import com.maverickstube.maverickshub.models.User;
+
+import java.io.IOException;
 
 public interface MediaService {
     UploadMediaResponse upload(UploadMediaRequest request);
@@ -11,4 +15,5 @@ public interface MediaService {
     Media getMediaBy(long id);
 
 
+    UpdateMediaResponse updateMedia(UpdateMediaRequest updateMediaRequest) throws IOException;
 }
