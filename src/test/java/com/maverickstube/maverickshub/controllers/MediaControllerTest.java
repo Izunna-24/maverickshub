4398b5849ac9e6e5a9abdb285d9d8d6bb17d0b03
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-05 17:48:45 +0100

    java

--- src/test/java/com/maverickstube/maverickshub/controllers/MediaControllerTest.java
+++ src/test/java/com/maverickstube/maverickshub/controllers/MediaControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockPart;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -19,6 +20,7 @@ import java.nio.file.Path;
 
 import static com.maverickstube.maverickshub.utils.TestUtils.TEST_VIDEO_LOCATION;
 import static com.maverickstube.maverickshub.utils.TestUtils.buildUploadMediaRequest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
@@ -39,17 +41,29 @@ class MediaControllerTest {
         try (InputStream inputStream = Files.newInputStream(Path.of(TEST_VIDEO_LOCATION))) {
             MultipartFile file = new MockMultipartFile("mediaFile", inputStream);
             mockMvc.perform(multipart("/api/v1/media")
-                            .file(file.getName(),file.getBytes())
-                            .part(new MockPart("userId","200".getBytes()))
-                            .part(new MockPart("description","test description".getBytes()))
-                            .part(new MockPart("category","ACTION".getBytes()))
+                            .file(file.getName(), file.getBytes())
+                            .part(new MockPart("userId", "200".getBytes()))
+                            .part(new MockPart("description", "test description".getBytes()))
+                            .part(new MockPart("category", "ACTION".getBytes()))
                             .contentType(MediaType.MULTIPART_FORM_DATA))
-                           .andExpect(status().isCreated())
-                           .andDo(print());
+                    .andExpect(status().isCreated())
+                    .andDo(print());
 
         } catch (Exception exception) {
             throw exception;
         }
-
     }
+
+        @Test
+        public void testGetMediaForUser(){
+            try{
+                mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/media?userId=200")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                        .andDo(print());
+            }catch (Exception exception){
+                assertThat(exception).isNull();
+            }
+        }
+
 }
