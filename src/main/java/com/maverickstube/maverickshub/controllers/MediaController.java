8784ff45f7e7922c79407bd0881317d7550acef9
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-05 15:28:42 +0100

    java

--- src/main/java/com/maverickstube/maverickshub/controllers/MediaController.java
+++ src/main/java/com/maverickstube/maverickshub/controllers/MediaController.java
@@ -1,5 +1,32 @@
 package com.maverickstube.maverickshub.controllers;
 
+
+import com.maverickstube.maverickshub.dataTransferObjects.requests.UploadMediaRequest;
+import com.maverickstube.maverickshub.dataTransferObjects.responses.UploadMediaResponse;
+import com.maverickstube.maverickshub.services.MediaService;
+import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
+@RestController
+@RequestMapping("/api/v1/media")
+@AllArgsConstructor
 public class MediaController {
+
+    private final MediaService mediaService;
+
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<UploadMediaResponse> uploadMedia(@ModelAttribute UploadMediaRequest uploadMediaRequest){
+     return ResponseEntity.status(CREATED)
+             .body(mediaService.upload(uploadMediaRequest));
+    }
 
 }
