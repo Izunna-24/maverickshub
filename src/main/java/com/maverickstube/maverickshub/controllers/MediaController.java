df13afc8cf51d8143a3183b6546ea10190ec9869
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-06 04:21:10 +0100

    java

--- src/main/java/com/maverickstube/maverickshub/controllers/MediaController.java
+++ src/main/java/com/maverickstube/maverickshub/controllers/MediaController.java
@@ -6,15 +6,11 @@ import com.maverickstube.maverickshub.dataTransferObjects.responses.MediaRespons
 import com.maverickstube.maverickshub.dataTransferObjects.responses.UploadMediaResponse;
 import com.maverickstube.maverickshub.services.MediaService;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 @RequestMapping("/api/v1/media")
