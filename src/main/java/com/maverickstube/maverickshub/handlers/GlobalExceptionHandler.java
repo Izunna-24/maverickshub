aa46cc20e81e00b640a91a423df16f9430f95558
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-10 09:12:24 +0100

    java

---
+++ src/main/java/com/maverickstube/maverickshub/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.maverickstube.maverickshub.handlers;
+
+
+
+import com.maverickstube.maverickshub.exceptions.MediaUploadFailedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+//@ControllerAdvice //used to send error pages when dealing with mvc apps
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MediaUploadFailedException.class)
+    @ResponseBody
+    public ResponseEntity<?> handleMediaUploadFailed(MediaUploadFailedException exception){
+        return ResponseEntity.status(BAD_REQUEST)
+                .body(Map.of("error", exception.getMessage(),
+                        "success",false));
+    }
+}
