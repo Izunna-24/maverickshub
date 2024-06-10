7eafbb11dbc3631901b787bed0d16372754bf44d
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-10 09:15:50 +0100

    java

--- src/main/java/com/maverickstube/maverickshub/services/MediaService.java
+++ src/main/java/com/maverickstube/maverickshub/services/MediaService.java
@@ -1,9 +1,12 @@
 package com.maverickstube.maverickshub.services;
 
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import com.maverickstube.maverickshub.dataTransferObjects.requests.UpdateMediaRequest;
 import com.maverickstube.maverickshub.dataTransferObjects.requests.UploadMediaRequest;
 import com.maverickstube.maverickshub.dataTransferObjects.responses.MediaResponse;
 import com.maverickstube.maverickshub.dataTransferObjects.responses.UpdateMediaResponse;
+import com.maverickstube.maverickshub.dataTransferObjects.responses.UpdateMediaResponse2;
 import com.maverickstube.maverickshub.dataTransferObjects.responses.UploadMediaResponse;
 import com.maverickstube.maverickshub.models.Media;
 import com.maverickstube.maverickshub.models.User;
@@ -20,4 +23,5 @@ public interface MediaService {
     UpdateMediaResponse updateMedia(UpdateMediaRequest updateMediaRequest) throws IOException;
 
     List<MediaResponse> getMediaFor(Long userId);
+    UpdateMediaResponse2 updateMedia2(Long mediaId, JsonPatch jsonPatch);
 }
