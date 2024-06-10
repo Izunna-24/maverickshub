dd11603f143d6faa34ebfdaed908d4d87fea285c
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-10 09:18:39 +0100

    java

--- src/main/java/com/maverickstube/maverickshub/models/User.java
+++ src/main/java/com/maverickstube/maverickshub/models/User.java
@@ -1,6 +1,10 @@
 package com.maverickstube.maverickshub.models;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -23,17 +27,21 @@ public class User {
     private String email;
     private String password;
     @Setter(AccessLevel.NONE)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime timeCreated;
     @Setter(AccessLevel.NONE)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime timeUpdated;
 
     @PrePersist
     private void setTimeCreated(){
-        this.timeCreated = now();
+        timeCreated = now();
     }
 
     @PreUpdate
     private void setTimeUpdated(){
-        this.timeUpdated = now();
+        timeUpdated = now();
     }
 }
